9bd349272e8d5ffb23e3287c15b43862642d96a0
NashedShahRoni22 <nashed15-11170@diu.edu.bd>
2022-11-08 04:43:16 +0600

    services data api setup done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,6 +10,37 @@ app.use(cors());
 app.use(express.json());
 require("dotenv").config();
 
+//mongobd connection
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.auieprw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const servicefCollection = client.db("chitromayaDb").collection("services");
+    //data limit 3
+    app.get("/servicesThree", async (req, res) => {
+      const query = {};
+      const cursor = servicefCollection.find(query);
+      const services = await cursor.limit(3).toArray();
+      res.send(services);
+    });
+    //no limit of loading data
+    app.get("/services", async (req, res) => {
+        const query = {};
+        const cursor = servicefCollection.find(query);
+        const services = await cursor.toArray();
+        res.send(services);
+      });
+  } finally {
+  }
+}
+
+run().catch((e) => console.error(e));
 
 app.get("/", (req, res) => {
   res.send("Chitromaya Server is running");
